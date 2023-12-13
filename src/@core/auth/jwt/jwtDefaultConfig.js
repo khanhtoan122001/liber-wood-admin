cfda3107506add58f3d29d8bfecbab396e5223f2
Toàn <71191923+khanhtoan122001@users.noreply.github.com>
2023-12-14 03:30:10 +0700

    init

---
+++ src/@core/auth/jwt/jwtDefaultConfig.js
@@ -0,0 +1,16 @@
+// ** Auth Endpoints
+const apiEndpoint = "http://127.0.0.1:8000"
+export default {
+  loginEndpoint: `${apiEndpoint}/oauth/token`,
+  registerEndpoint: '/jwt/register',
+  refreshEndpoint: '/jwt/refresh-token',
+  logoutEndpoint: '/jwt/logout',
+
+  // ** This will be prefixed in authorization header with token
+  // ? e.g. Authorization: Bearer <token>
+  tokenType: 'Bearer',
+
+  // ** Value of this property will be used as key to store JWT token in storage
+  storageTokenKeyName: 'accessToken',
+  storageRefreshTokenKeyName: 'refreshToken'
+}
